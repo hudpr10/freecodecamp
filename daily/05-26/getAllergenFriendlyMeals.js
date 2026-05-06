2d7943973eefd13321c5ba363d50567d56bce6ab
Hudson Reis <95765155+hudpr10@users.noreply.github.com>
2026-05-06 16:13:05 -0300

    :sparkles: add: 06-05
    Implement function to filter meals based on allergens.

---
+++ daily/05-26/getAllergenFriendlyMeals.js
@@ -0,0 +1,23 @@
+/*
+Allergen Friendly Meals
+
+Given an array of meals and an array of allergens to avoid, return the names of all the meals that contain none of the given allergens.
+
+  - Each meal is in the format [meal, allergens], where meal is the name of the meal, and allergens is an array of the allergens the meal contains. For example, ["pasta", ["wheat", "milk"]].
+  - Allergens to avoid will be an array of strings.
+  
+Return safe meal names in the same order given. If no meal is safe, return an empty array.
+*/
+
+function getAllergenFriendlyMeals(meals, allergens) {
+  const allergensSet = new Set(allergens);
+    
+  return meals
+    .filter(([meal, ingredients]) => 
+            ingredients.every(ingredient => 
+                              !allergensSet.has(ingredient)))
+    .map(([name]) => name);
+}
+
+getAllergenFriendlyMeals([["oatmeal", ["nuts"]], ["pancakes", ["wheat", "milk"]], ["granola", []], ["yogurt", ["milk"]], ["eggs", ["eggs", "milk"]], ["toast", ["wheat"]]], ["eggs", "milk"]);
+// return: [ 'oatmeal', 'granola', 'toast' ]
